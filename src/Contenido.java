463455c620cfbb7e6f5f6e0904000dcbd1c3cdd2
NurysOchoa <nurys8a@gmail.com>
2021-04-02 21:31:36 -0300

    Subo mi primer proyecto de OOP Metflix

---
+++ src/Contenido.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Contenido {
+    public String nombre;
+    public Genero genero;
+    //Idioma Origen
+    public Idioma idiomaOrigen;
+    public int añoLanzamiento;
+    public List<Actor> actores = new ArrayList<>();
+
+}
